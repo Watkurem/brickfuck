cf5174dd12e36d76b801421d42439c69115b8dec
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-05-15 13:55:54 +0300

    Add dummy source
    Introduces bf_tape array of long longs, classic 30`000 initialized to
    zero. Also introduces pointer bf_ptr that will be used for selection of
    specific array member.

---
+++ brickfuck.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	long long bf_tape[30000] = {0};
+	long long *bf_ptr = bf_tape;
+
+	if(argc <= 1){
+		puts("Specify brainfucksource file.");
+	}
+	return 0;
+}
